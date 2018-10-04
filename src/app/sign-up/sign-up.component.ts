856ed3d52cb6af543f7205ed16639f07e0ec2c2e
Jérémy GUIMARD <jeremy.guimard.pro@gmail.com>
2018-10-04 11:53:28 +0200

    Angular refresh

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+   email:string = "Enter your email";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
